0fa33240366a0596158b0cca80eb595dcc21d395
marco.livesu@ge.imati.cnr.it <marco.livesu@ge.imati.cnr.it>
2022-02-10 23:32:10 +0100

    must fix the last vertex index

--- main.cpp
+++ main.cpp
@@ -7,13 +7,37 @@ using namespace cinolib;
 
 //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
+void IGM(const uint genus, DrawableQuadmesh<> & igm)
+{
+    uint c = igm.vert_add(vec3d(0,0,0));
+    std::vector<vec3d> verts = n_sided_polygon(4*genus, CIRCLE);
+    for(const auto & p : verts) igm.vert_add(p);
+
+    for(uint i=0; i<genus; ++i)
+    {
+        uint off = 4*i;
+        uint v0 = igm.vert_add((verts[off+1] + igm.vert(c) + verts[off+2])/3);
+        uint v1 = igm.vert_add(0.70*verts[off+2] + 0.30*igm.vert(c));
+        uint v2 = igm.vert_add((verts[off+2] + igm.vert(c) + verts[off+3])/3);
+
+        igm.poly_add({off+1, off+2, v0,  c});
+        igm.poly_add({off+2, off+3, v1, v0});
+        igm.poly_add({off+3, off+4, v2, v1});
+        igm.poly_add({off+4, (off+4)%igm.num_verts()+1,  c, v2});
+        igm.poly_add({   v0,    v1, v2,  c});
+    }
+
+    igm.updateGL();
+}
+
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
 int main()
 {
-    uint genus = 2;
-    std::vector<vec3d> verts = n_sided_polygon(4*genus, CIRCLE);
-    DrawablePolygonmesh<> m(verts);
+    DrawableQuadmesh<> m;
+    IGM(3,m);
     GLcanvas gui;
-    SurfaceMeshControls<DrawablePolygonmesh<>> controls(&m,&gui,"Polygonal Schema");
+    SurfaceMeshControls<DrawableQuadmesh<>> controls(&m,&gui,"IGM");
     gui.push(&m);
     gui.push(&controls);
     return gui.launch();
