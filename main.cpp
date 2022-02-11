e9623237a8003fee1a5220030ff47ad687263d73
marco.livesu@ge.imati.cnr.it <marco.livesu@ge.imati.cnr.it>
2022-02-11 08:39:34 +0100

    cleaned IGM

--- main.cpp
+++ main.cpp
@@ -9,22 +9,35 @@ using namespace cinolib;
 
 void IGM(const uint genus, DrawableQuadmesh<> & igm)
 {
-    uint c = igm.vert_add(vec3d(0,0,0));
     std::vector<vec3d> verts = n_sided_polygon(4*genus, CIRCLE);
     for(const auto & p : verts) igm.vert_add(p);
+    uint origin = igm.vert_add(vec3d(0,0,0)); // center point
 
     for(uint i=0; i<genus; ++i)
     {
-        uint off = 4*i;
-        uint v0 = igm.vert_add((verts[off+1] + igm.vert(c) + verts[off+2])/3);
-        uint v1 = igm.vert_add(0.70*verts[off+2] + 0.30*igm.vert(c));
-        uint v2 = igm.vert_add((verts[off+2] + igm.vert(c) + verts[off+3])/3);
+        // 5 consecutive corners of the Canonical Polygonal Schema
+        uint cps[5] =
+        {
+            4*i,
+            4*i+1,
+            4*i+2,
+            4*i+3,
+           (4*i+4)%(genus*4)
+        };
 
-        igm.poly_add({off+1, off+2, v0,  c});
-        igm.poly_add({off+2, off+3, v1, v0});
-        igm.poly_add({off+3, off+4, v2, v1});
-        igm.poly_add({off+4, (off+4)%igm.num_verts()+1,  c, v2});
-        igm.poly_add({   v0,    v1, v2,  c});
+        // auxiliary inner vertices
+        uint inn[3] =
+        {
+            igm.vert_add((verts[cps[1]] + verts[cps[2]] + igm.vert(origin))/3),
+            igm.vert_add(0.7*verts[cps[2]] + 0.3*igm.vert(origin)),
+            igm.vert_add((verts[cps[2]] + verts[cps[3]] + igm.vert(origin))/3)
+        };
+
+        igm.poly_add({cps[0], cps[1], inn[0], origin});
+        igm.poly_add({cps[1], cps[2], inn[1], inn[0]});
+        igm.poly_add({cps[2], cps[3], inn[2], inn[1]});
+        igm.poly_add({cps[3], cps[4], origin, inn[2]});
+        igm.poly_add({inn[0], inn[1], inn[2], origin});
     }
 
     igm.updateGL();
